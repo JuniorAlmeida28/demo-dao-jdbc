d4cf4471244baeb15fea2b5807b11a08a1938c60
Junior Almeida <jrjunioralmeida@outlook.com>
2023-09-27 12:40:15 -0300

    fim

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,4 @@
-package aplication;
+package application;
 
 import java.util.Date;
 import java.util.List;
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -2,65 +2,152 @@ package model.dao.impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
-public class DepartmentDaoJDBC implements DepartmentDao{
+public class DepartmentDaoJDBC implements DepartmentDao {
 
-    private Connection conn;
+	private Connection conn;
+	
+	public DepartmentDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
+	@Override
+	public Department findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+				"SELECT * FROM department WHERE Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Department obj = new Department();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				return obj;
+			}
+			return null;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
 
-    public DepartmentDaoJDBC(Connection conn) {
-        this.conn = conn;
-    }
+	@Override
+	public List<Department> findAll() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+				"SELECT * FROM department ORDER BY Name");
+			rs = st.executeQuery();
 
-    @Override
-    public void deleteById(Integer id) {
-        PreparedStatement st = null;
-        try {
-            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			List<Department> list = new ArrayList<>();
 
-            st.setInt(1, id);
+			while (rs.next()) {
+				Department obj = new Department();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				list.add(obj);
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
 
-            int rows = st.executeUpdate();
+	@Override
+	public void insert(Department obj) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+				"INSERT INTO department " +
+				"(Name) " +
+				"VALUES " +
+				"(?)", 
+				Statement.RETURN_GENERATED_KEYS);
 
-            if (rows == 0){
-                throw new DbException("Id inexistente");
-            }
-        } catch (SQLException e) {
-            throw new DbException(e.getMessage());
-        }
-        finally{
-            DB.closeStatement(st);
-        }
-    }
+			st.setString(1, obj.getName());
 
-    @Override
-    public List<Department> findAll() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+			}
+			else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+		}
+	}
 
-    @Override
-    public Department findById(Integer id) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public void update(Department obj) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+				"UPDATE department " +
+				"SET Name = ? " +
+				"WHERE Id = ?");
 
-    @Override
-    public void insert(Department obj) {
-        // TODO Auto-generated method stub
-        
-    }
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
 
-    @Override
-    public void update(Department obj) {
-        // TODO Auto-generated method stub
-        
-    }
-    
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+		}
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+				"DELETE FROM department WHERE Id = ?");
+
+			st.setInt(1, id);
+
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+		}
+	}
 }
